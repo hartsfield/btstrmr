cbf2f75fcbc9c9eba6b9320f652dd581914c797c
bitco <anonymous15415@gmail.com>
2016-04-15 15:03:03 -0400

    fixed dupkey bug

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   secret: '$3G43g5$H^5H5^_99[=GMiy(*l)',
-  database: 'mongodb://localhost:27017/btco',
+  database: 'mongodb://localhost:27017/btco3',
   port: '3400',
   ip: 'localhost',
 }
--- models/user.js
+++ models/user.js
@@ -5,12 +5,12 @@ mongoose.connect(config.database, function() {
   console.log("connected to mongodb!")
 });
 module.exports = mongoose.model('User', new Schema({
-  name       : {type: String, unique : true, required : true, dropDups: true},
+  name       : {type: String, unique : true, required : true, },
   password   : {type: String, required : true},
   email      : {type: String},
   karma      : {type: Number, default: 0},
   heard      : [String],
-  liked      : {type: Array, unique: true, default: ["0"]},
+  liked      : [String],
   submissions: [String],
   created    : {type: Date, default: Date.now}
 }));
