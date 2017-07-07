9821945416ea0a9b21732cf8971f4896a7fc0c9b
Johnathan Hartsfield <johnathanhartsfield@gmail.com>
2017-07-07 17:26:07 +0000

    adding licenses

--- models/audio.js
+++ models/audio.js
@@ -1,13 +1,37 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2017 J. Hartsfield
+                                                                               
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+                                                                               
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+                                                                               
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////
+
+// The Audio schema is the model for our track data
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
 module.exports = mongoose.model('Audio', new Schema({
-  Artist : {type: String},
-  Album  : {type: String},
-  Title  : {type: String},
-  Audio  : {type: String},
-  Image  : {type: String},
-  Year   : {type: Number},
-  Likes  : {type: Number, default: 0},
-  Listens: {type: Number, default: 0},
-  Posted : {type: Date,   default: Date.now}
+  Artist : {type: String}, // Artists name
+  Album  : {type: String}, // Not implemented yet
+  Title  : {type: String}, // Title of track
+  Audio  : {type: String}, // The path to the audio data in the file system
+  Image  : {type: String}, // The path to the image data in the file system
+  Year   : {type: Number}, // Year song was released
+  Likes  : {type: Number, default: 0}, // Number of "likes"
+  Listens: {type: Number, default: 0}, // Not implemented yet
+  Posted : {type: Date,   default: Date.now} // Date posted
 }));
--- models/user.js
+++ models/user.js
@@ -1,5 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2017 J. Hartsfield
+                                                                               
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+                                                                               
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+                                                                               
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+///////////////////////////////////////////////////////////////////////////////
+
+// The User schema is the model for our user data
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
 module.exports = mongoose.model('User', new Schema({
   name       : {type: String,
                 minlength: 4,
@@ -11,13 +35,17 @@ module.exports = mongoose.model('User', new Schema({
                 minlength: 4,
                 required : true,
                },
+  // Not required yet, but could come in useful
   email      : {type: String,
                 minlength: 4,
                 maxlength: 15,
                },
+  // Eventually users will be able to upload songs and earn karma
   karma      : {type: Number, default: 0},
+  // Not implemented yet, but this feature will allow users to trac all the
+  // songs they listen to.
   heard      : [String],
   liked      : [String],
-  submissions: [String],
-  created    : {type: Date, default: Date.now}
+  submissions: [String], // Not implmented yet
+  created    : {type: Date, default: Date.now} // Date account was created 
 }));
